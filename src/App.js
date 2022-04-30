2666dc86397faeb208470c9566aa0ad09b87fa90
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-01 03:20:17 +0600

    login and registration form done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home';
+import ManageProducts from './components/ManageProducts/ManageProducts';
 import Nav from './components/Nav/Nav';
-
+import Login  from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      <Home></Home>
+      <Routes>
+        <Route path="/"  element={<Home></Home>}/>
+        <Route path="/home" element={<Home></Home>}/>
+        <Route path="login" element ={<Login></Login>}/>
+        <Route path="manageProducts" element={<ManageProducts></ManageProducts>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,18 +7,21 @@ const Home = () => {
     const [products, setProducts] = useState([])
     useEffect(() => {
         axios.get(' https://pure-citadel-40053.herokuapp.com/').then(resp => {
-            setProducts(resp.data)
+            setProducts(resp.data.slice(0,3))
         });
     }, [])
        
     return (
-        <div>
+        <div className="">
             <Banner></Banner>
+            <div className="w-9/12 mx-auto">
             <h2 className="text-center text-3xl font-semibold my-4" >Browse Our Range</h2>
-            <div className="md:grid md:grid-cols-2 lg:grid-cols-3 w-9/12 mx-auto gap-6  mt-4">
+            <div className="md:grid md:grid-cols-2 lg:grid-cols-3  gap-6  mt-4">
                 {
-                    products.map(product =><Products product={product}></Products>)
+                    products.map(product =><Products key={product._id} product={product}></Products>)
                 }
+            </div>
+            <button className="btn btn-primary w-full my-10 ">Manage all Products</button>
             </div>
         </div>
     );
